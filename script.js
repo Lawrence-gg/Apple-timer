0ebd895328a692306e2b1a27025b68c2f6515f79
Lawrence-gg <Lawrence.goodhead@gmail.com>
2022-03-12 18:28:45 +1300

    query selectors added to javascript file

---
+++ script.js
@@ -0,0 +1,8 @@
+const status = document.querySelector('.timer-status');
+const minutes = document.querySelector('.timer-minutes');
+const seconds = document.querySelector('.timer-seconds');
+
+const start = document.querySelector('.timer-mode-start');
+const pause = document.querySelector('.timer-mode-pause');
+const reset = document.querySelector('.timer-mode-reset');
+const breakTimer = document.querySelector('.timer-mode-break');
