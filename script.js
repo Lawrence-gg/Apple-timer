a9a311226c39908f41c7e4632c361bb689132e8c
Lawrence-gg <Lawrence.goodhead@gmail.com>
2022-03-12 22:29:26 +1300

    timer control buttons functionality implemented

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let counting = false;
+let counting = undefined;
 
 const status = document.querySelector('.timer-status');
 const minutes = document.querySelector('.timer-minutes');
@@ -9,21 +9,32 @@ const pauseBtn = document.querySelector('.timer-mode-pause');
 const resetBtn = document.querySelector('.timer-mode-reset');
 const breakTimeBtn = document.querySelector('.timer-mode-break');
 
+//Starting the timer;
 startBtn.addEventListener('click', () => {
-  if (!counting) {
-    startTimer();
-    counting = true;
-  } else {
-    return;
-  }
+  if (counting == undefined) counting = setInterval(timer, 1000);
 });
 
-//Starting the timer;
-function startTimer() {
-  setInterval(timer, 1000);
+pauseBtn.addEventListener('click', () => {
+  counting = clearInterval(counting);
+});
+
+resetBtn.addEventListener('click', () => {
+  counting = clearInterval(counting);
+  resetTimer();
+});
+
+breakTimeBtn.addEventListener('click', () => {
+  counting = clearInterval(counting);
+  minutes.innerHTML = '5';
+  seconds.innerHTML = '00';
+});
+
+function resetTimer() {
+  minutes.innerHTML = '25';
+  seconds.innerHTML = '00';
 }
 
-//Co
+//Timer clock function
 function timer() {
   let currentMinutes = minutes.innerHTML;
   let currentSeconds = seconds.innerHTML;
@@ -37,17 +48,19 @@ function timer() {
     }
   } else if (currentMinutes == 0 && currentSeconds != 0) {
     currentSeconds--;
+  } else {
+    return;
   }
   updateTime(currentMinutes, currentSeconds);
 }
 
-function updateTime(min, sec) {
-  minutes.innerHTML = min;
+function updateTime(currentMinutes, currentSeconds) {
+  minutes.innerHTML = currentMinutes;
   if (currentSeconds < 10) {
-    seconds.innerHTML = `0${sec}`;
-    document.title = `${min}:0${sec}`;
+    seconds.innerHTML = `0${currentSeconds}`;
+    document.title = `${currentMinutes}:0${currentSeconds}`;
   } else {
-    seconds.innerHTML = sec;
-    document.title = `${min}:${sec}`;
+    seconds.innerHTML = currentSeconds;
+    document.title = `${currentMinutes}:${currentSeconds}`;
   }
 }
