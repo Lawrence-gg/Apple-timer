89f01833099fd430ba50b7bcd2867344f40e8d7b
Lawrence-gg <Lawrence.goodhead@gmail.com>
2022-03-12 21:52:12 +1300

    Add button styling

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ const status = document.querySelector('.timer-status');
 const minutes = document.querySelector('.timer-minutes');
 const seconds = document.querySelector('.timer-seconds');
 
-const start = document.querySelector('.timer-mode-start');
-const pause = document.querySelector('.timer-mode-pause');
-const reset = document.querySelector('.timer-mode-reset');
-const breakTimer = document.querySelector('.timer-mode-break');
+const startBtn = document.querySelector('.timer-mode-start');
+const pauseBtn = document.querySelector('.timer-mode-pause');
+const resetBtn = document.querySelector('.timer-mode-reset');
+const breakTimeBtn = document.querySelector('.timer-mode-break');
 
-setInterval(timer, 1000);
+//Starting the timer;
+function startTimer() {
+  setInterval(timer, 1000);
+}
 
 function timer() {
   let currentMinutes = minutes.innerHTML;
@@ -16,7 +19,7 @@ function timer() {
   if (currentMinutes != 00) {
     if (currentSeconds == 0) {
       currentMinutes--;
-      currentSeconds = 4;
+      currentSeconds = 59;
     } else {
       currentSeconds--;
     }
