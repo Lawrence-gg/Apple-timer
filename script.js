464f85b0166b33ac82a5719459f159dea65c824a
Lawrence-gg <Lawrence.goodhead@gmail.com>
2022-03-12 18:54:43 +1300

    Implemented timer function

--- script.js
+++ script.js
@@ -6,3 +6,24 @@ const start = document.querySelector('.timer-mode-start');
 const pause = document.querySelector('.timer-mode-pause');
 const reset = document.querySelector('.timer-mode-reset');
 const breakTimer = document.querySelector('.timer-mode-break');
+
+setInterval(timer, 1000);
+
+function timer() {
+  let currentMinutes = minutes.innerHTML;
+  let currentSeconds = seconds.innerHTML;
+
+  if (currentMinutes != 00) {
+    if (currentSeconds == 0) {
+      currentMinutes--;
+      currentSeconds = 4;
+    } else {
+      currentSeconds--;
+    }
+  } else if (currentMinutes == 0 && currentSeconds != 0) {
+    currentSeconds--;
+  }
+
+  minutes.innerHTML = currentMinutes;
+  seconds.innerHTML = currentSeconds;
+}
